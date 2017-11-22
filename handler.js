a1a04dcc5c9537904ba6f9d8d63f6575084f695a
Hans-Henrik Ravn <hhravn@gmail.com>
2017-11-22 23:19:48 +0100

    first commit

---
+++ handler.js
@@ -0,0 +1,44 @@
+'use strict';
+const fetch = require('node-fetch');
+const _get = require('lodash/get');
+
+const AWS = require('aws-sdk');
+
+require('dotenv').config()
+
+module.exports.poll = (event, context, callback) => {
+  fetch(process.env.SOURCE_URL)
+    .then((res) => {
+      res
+        .json()
+        .then((json) => {
+          const temperature = _get(json, 'feeds[0].field1', 0);
+          const date = _get(json, 'feeds[0].created_at', "1970-01-01T00:00:00Z");
+          const timestamp = new Date(date).getTime();
+          return { temperature, timestamp }
+        })
+        .then((result) => {
+          if(result.temperature > process.env.THRESHOLD_TEMP) {
+            const sns = new AWS.SNS();
+
+            const time = new Date(result.timestamp);
+
+            const params = {
+              Subject: `Temperature High: ${result.temperature}`,
+              Message: `The latest temperature reading from ${time.toISOString()} is ${result.temperature}`,
+              TopicArn: process.env.SNS_TOPIC
+            };
+
+            sns.publish(params, (error, data) => {
+              if (error) {
+                callback(error);
+              }
+              callback(null, result);
+            });
+          } else {
+            callback(null, result)
+          }
+        });
+    })
+    .catch((err) => callback(err, null));
+};
